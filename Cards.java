2ff78ad4a23edc2f02ef2d76755cd01be6006634
Hettgen <hettgen49@gmail.com>
2023-06-23 13:45:49 -0400

    Added base code provided

--- Cards.java
+++ Cards.java
@@ -2,10 +2,36 @@
 
 public class Cards{
 
-    public static void main(String[] args) {
-        
-        System.out.println("hello");
+    private String suit; //clubs, spades, diamonds, hearts
+   private int value;//1-13
 
+   public static final String [] SUITS = {"Heartt", "Diamonds", "Spades", "Clubs"};
+    /**
+     * @return the suit
+     */
+    public String getSuit() {
+        return suit;
+    }
+
+    /**
+     * @param suit the suit to set
+     */
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    /**
+     * @return the value
+     */
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(int value) {
+        this.value = value;
     }
 
 }
